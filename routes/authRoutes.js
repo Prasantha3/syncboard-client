d4a56e1e3d9a607842404b27e16a09df223b4512
Prasantha3 <prasageek.lk@gmail.com>
2026-08-29 21:53:34 +0530

    Refactor authRoutes to use controller methods

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,9 @@
 import express from 'express';
+import { register, login } from '../controllers/authController.js';
+
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  console.log('BODY RECEIVED:', req.body);
-  const { username, email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Email and password required' });
-  }
-
-  res.status(201).json({
-    message: 'User registered successfully',
-    user: { username, email }
-  });
-});
-
-router.get('/', (req, res) => {
-  res.send('auth ok');
-});
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
